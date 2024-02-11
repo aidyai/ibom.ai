2c5c8dfdb178cb160a0f4b9345f818d196a67cbe
aidyai <aidyosu553@gmail.com>
2024-02-11 13:59:48 -0800

    v0.m

--- api/main.py
+++ api/main.py
@@ -78,9 +78,9 @@ def api_tts(text_request: TextRequest):
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
-
-
 #if __name__ == "__main__":
-#    uvicorn.run("src.main:app", host="0.0.0.0", port=10000, reload=True)
+#    uvicorn.run("main:app", port=8000, reload=True)
+
+
+if __name__ == "__main__":
+    uvicorn.run("src.main:app", host="0.0.0.0", port=10000, reload=True)
