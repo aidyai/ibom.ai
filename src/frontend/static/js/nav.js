ae5b3849856162d15bd6f4e3c75343a031d4a8d1
aidyai <aidyosu553@gmail.com>
2024-02-26 17:08:21 -0800

    v.3.0

--- src/frontend/static/js/nav.js
+++ src/frontend/static/js/nav.js
@@ -3,6 +3,9 @@ const dropdown = document.querySelectorAll(".dropdown");
 const hamburgerBtn = document.getElementById("hamburger");
 const navMenu = document.querySelector(".menu");
 const links = document.querySelectorAll(".dropdown a");
+const loginBtn = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signupBtn");
+const modalContainer = document.getElementById("modalContainer");
 
 function setAriaExpandedFalse() {
   dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
@@ -17,7 +20,35 @@ function closeDropdownMenu() {
 
 function toggleHamburger() {
   navMenu.classList.toggle("show");
+  if (navMenu.classList.contains("show")) {
+    // If hamburger menu is opened, append login and signup buttons
+    navMenu.appendChild(loginBtn);
+    navMenu.appendChild(signupBtn);
+  } else {
+    // If hamburger menu is closed, move login and signup buttons back to their original position
+    document.querySelector('.right-container').appendChild(loginBtn);
+    document.querySelector('.right-container').appendChild(signupBtn);
+  }
 }
+
+function hideNavLinks() {
+  navMenu.classList.remove("show");
+}
+
+// Add event listeners to the login and signup buttons
+loginBtn.addEventListener('click', hideNavLinks);
+signupBtn.addEventListener('click', hideNavLinks);
+
+// Toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
+
+function hideNavLinks() {
+  navMenu.classList.remove("show");
+}
+
+// Add event listeners to the login and signup buttons
+loginBtn.addEventListener('click', hideNavLinks);
+signupBtn.addEventListener('click', hideNavLinks);
 
 dropdownBtn.forEach((btn) => {
   btn.addEventListener("click", function (e) {
@@ -38,7 +69,7 @@ dropdownBtn.forEach((btn) => {
   });
 });
 
-// close dropdown menu when the dropdown links are clicked
+// Close dropdown menu when the dropdown links are clicked
 links.forEach((link) =>
   link.addEventListener("click", () => {
     closeDropdownMenu();
@@ -47,13 +78,13 @@ links.forEach((link) =>
   })
 );
 
-// close dropdown menu when you click on the document body
+// Close dropdown menu when you click on the document body
 document.documentElement.addEventListener("click", () => {
   closeDropdownMenu();
   setAriaExpandedFalse();
 });
 
-// close dropdown when the escape key is pressed
+// Close dropdown when the escape key is pressed
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     closeDropdownMenu();
@@ -61,5 +92,6 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-// toggle hamburger menu
-hamburgerBtn.addEventListener("click", toggleHamburger);
+
+
+
