8b00c51aeedafd324bd0ee314aa409b08d6bd476
aidyai <aidyosu553@gmail.com>
2024-03-05 17:25:25 -0800

    app.v2

--- src/utils/base.py
+++ src/utils/base.py
@@ -13,7 +13,7 @@ BASE_DIR = pathlib.Path(__file__).parent
 JSON_FILENAME = 'objects/dict5k_.json'
 JSON_FILE_PATH = BASE_DIR / JSON_FILENAME
 
-CREDENTIALS = BASE_DIR / 'objects/auth.json'
+CREDENTIALS = BASE_DIR / 'objects/vauth.json'
 
 
 # Access the environment variables
