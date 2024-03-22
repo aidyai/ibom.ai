dbffcc4c85531844aa953fd9920a467d6013afea
aidyai <aidyosu553@gmail.com>
2024-03-22 10:46:42 -0700

    v.3.4

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import uvicorn
 #from src import main
 
 
-##if __name__ == "__main__":
+#if __name__ == "__main__":
 #    uvicorn.run("src.main:app", port=8000, reload=True)
 
 if __name__ == "__main__":
