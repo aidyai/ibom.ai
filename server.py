5a87c5515ae99b4e1fa88a4d467dd727435e6bd4
aidyai <aidyosu553@gmail.com>
2024-04-15 01:21:13 -0700

    V5

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-import uvicorn
 #from src import main
+import os
+import uvicorn
+
+
 
 
 #if __name__ == "__main__":
 #  uvicorn.run("src.main:app", port=8000, reload=True)
 
-if __name__ == "__main__":
-  uvicorn.run("src.main:app", host="0.0.0.0", port=10000, reload=True)
+PORT = int(10000)
+HOST = '0.0.0.0'
 
-      
+if __name__ == "__main__":
+  uvicorn.run("src.main:app", host=HOST, port=PORT, reload=True)
+
+#if __name__ == "__main__":
+#  uvicorn.run("src.main:app", host=HOST, port=10000, reload=True)
